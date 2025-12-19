6798c1273d596466e374959681cd26f86aca9afb
Daniel Romero <daniantonioara18@gmail.com>
2025-12-19 16:48:24 -0300

    Buscar Error, por consola

--- proyecto_integracion/settings.py
+++ proyecto_integracion/settings.py
@@ -43,6 +43,26 @@ INSTALLED_APPS = [
     'Barberia'
 ]
 
+LOGGING ={
+    'version':1,
+    'disable_existing_loggers': False,
+    'handlers':{
+        'console':{'class':'logging.StreamHandler'},
+    },
+    "root":{
+        'handlers': ['console'],
+        'level':'INFO',
+    },
+    'loggers':{
+        'django.request':{
+            'handlers':['console'],
+            'level':'ERROR',
+            'proppgate': True,
+        },
+    },
+
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
